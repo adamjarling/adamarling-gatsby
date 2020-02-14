1b0e50c891998c0e165ae25d0c15395cad76d337
Adam Arling <adam.arling@northwestern.edu>
2020-02-13 20:48:03 -0600

    Heavy customization to front page, building out portfolio

--- config.js
+++ config.js
@@ -1,36 +1,39 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter SolidState', // <title>
+  siteTitle: 'Adam J. Arling', // <title>
   manifestName: 'SolidState',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
+  // manifestBackgroundColor: '#663399',
+  // manifestThemeColor: '#663399',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Adam J. Arling',
+  subHeading:
+    'Recording Engineer, Live & Studio Musician, UI/JavaScript developer',
   // social
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/adamjarling',
     },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://facebook.com/adamarling',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:aarling@gmail.com',
     },
   ],
   phone: '000-00000',
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 
-import pic1 from '../assets/images/pic01.jpg';
-import pic2 from '../assets/images/pic02.jpg';
+import pic1 from '../assets/images/ABBARAMA.jpg';
+import pic2 from '../assets/images/adam-rd-square.jpg';
 import pic3 from '../assets/images/pic03.jpg';
 import pic4 from '../assets/images/pic04.jpg';
 import pic5 from '../assets/images/pic05.jpg';
 import pic6 from '../assets/images/pic06.jpg';
 import pic7 from '../assets/images/pic07.jpg';
+import portfolioTradeR from '../assets/images/portfolio-trade-r.jpg';
+import daredevil from '../assets/images/portfolio-daredevil.jpg';
+import portfolioNU from '../assets/images/portfolio-nulib.jpg';
+import portfolioABBA from '../assets/images/portfolio-abbarama.jpg';
+import portfolioTLV from '../assets/images/portfolio-thelastvegas.jpg';
+import portfolioOSD from '../assets/images/portfolio-osd-viewer.jpg';
+import portfolioUO from '../assets/images/portfolio-urge-overkill.jpg';
+import portfolioBlackAngus from '../assets/images/portfolio-blackangus.jpg';
+import portfolioAvalon from '../assets/images/portfolio-avalon.jpg';
+import portfolioArch from '../assets/images/portfolio-arch.jpg';
+import portfolioMorningstar from '../assets/images/portfolio-morningstar.jpg';
 
 import config from '../../config';
 const IndexPage = () => (
@@ -16,7 +27,7 @@ const IndexPage = () => (
     <section id="banner">
       <div className="inner">
         <div className="logo">
-          <span className="icon fa-diamond"></span>
+          <span className="icon fa-bolt"></span>
         </div>
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
@@ -24,34 +35,28 @@ const IndexPage = () => (
     </section>
 
     <section id="wrapper">
-      <section id="one" className="wrapper spotlight style1">
+      {/* <section id="one" className="wrapper spotlight style1">
         <div className="inner">
           <a href="/#" className="image">
             <img src={pic1} alt="" />
           </a>
           <div className="content">
-            <h2 className="major">Magna arcu feugiat</h2>
-            <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
-            </p>
+            <h2 className="major">Recent web projects</h2>
+            <p>Moving everthing to Netlify, and Netlify CMS, yadda yadda</p>
             <a href="/#" className="special">
               Learn more
             </a>
           </div>
         </div>
-      </section>
+      </section> */}
 
-      <section id="two" className="wrapper alt spotlight style2">
+      {/* <section id="two" className="wrapper alt spotlight style2">
         <div className="inner">
           <a href="/#" className="image">
             <img src={pic2} alt="" />
           </a>
           <div className="content">
-            <h2 className="major">Tempus adipiscing</h2>
+            <h2 className="major">Performing and recording</h2>
             <p>
               Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
               turpis ante, nullam sit amet turpis non, sollicitudin posuere
@@ -64,9 +69,9 @@ const IndexPage = () => (
             </a>
           </div>
         </div>
-      </section>
+      </section> */}
 
-      <section id="three" className="wrapper spotlight style3">
+      {/* <section id="three" className="wrapper spotlight style3">
         <div className="inner">
           <a href="/#" className="image">
             <img src={pic3} alt="" />
@@ -85,67 +90,195 @@ const IndexPage = () => (
             </a>
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section id="four" className="wrapper alt style1">
         <div className="inner">
-          <h2 className="major">Vitae phasellus</h2>
+          <h2 className="major">Portfolio</h2>
           <p>
-            Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-            Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-            egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus
-            in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
+            Innovative and proactive Front-End Developer capable of developing
+            component-based, JavaScript web applications for demanding clients.
+            Talented in building front-end stacks, team leadership and
+            independent problem-solving. Highly organized multitasker with
+            expertise in scheduling projects, enhancing designs and verifying
+            code. Prepared to offer React, JavaScript and UX abilities in
+            challenging roles.
+          </p>
+          <p>
+            I currently work for{' '}
+            <a href="https://www.library.northwestern.edu/" target="_blank">
+              Northwestern University Libraries Repository and Digital Curation
+              department,
+            </a>{' '}
+            writing open-source, repository applications which digitize the
+            world's cultural heritage assets.
           </p>
           <section className="features">
             <article>
-              <a href="/#" className="image">
-                <img src={pic4} alt="" />
+              <a href="https://www.abbarama.com/" className="image">
+                <img src={portfolioABBA} alt="" />
               </a>
-              <h3 className="major">Sed feugiat lorem</h3>
+              <h3 className="major">ABBARAMA</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                GatsbyJS, React, and Netlify CMS power this new website for pop
+                sensation ABBARAMA.
               </p>
-              <a href="/#" className="special">
+              <a href="https://www.abbarama.com/" className="special">
                 Learn more
               </a>
             </article>
             <article>
               <a href="/#" className="image">
-                <img src={pic5} alt="" />
+                <img src={portfolioTradeR} alt="Portfolio Trade-r screenshot" />
               </a>
-              <h3 className="major">Nisl placerat</h3>
+              <h3 className="major">Trade-R</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                <a href="https://trade-r.com">trade-r.com</a>
+                <br />
+                Cryptocurrency trading PWA which enforces disciplined trading
+                entrances and exits. Built from the ground up with React,
+                Firebase suite and various crypto APIs. Launch February 2020.
               </p>
-              <a href="/#" className="special">
+              <a href="https://trade-r.com" className="special">
                 Learn more
               </a>
             </article>
             <article>
-              <a href="/#" className="image">
-                <img src={pic6} alt="" />
+              <a href="https://www.daredevilpedals.com/" className="image">
+                <img src={daredevil} alt="" />
               </a>
-              <h3 className="major">Ante fermentum</h3>
+              <h3 className="major">Daredevil Pedals</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                Chicago-based fuzz pedal company making the raddest hand-wired
+                fuzz and delay pedals on the market. Check 'em out and fuzz on.
               </p>
-              <a href="/#" className="special">
+              <a href="https://www.daredevilpedals.com/" className="special">
                 Learn more
               </a>
             </article>
             <article>
-              <a href="/#" className="image">
-                <img src={pic7} alt="" />
+              <a href="https://dc.library.northwestern.edu/" className="image">
+                <img src={portfolioNU} alt="" />
               </a>
-              <h3 className="major">Fusce consequat</h3>
+              <h3 className="major">
+                Northwestern Libraries Digital Collections
+              </h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                React/Redux discovery application architected from the
+                ground-up. Hits an AWS Elasticsearch index, reads IIIF
+                manifests, and utilizes ReactiveSearch for faceting &amp;
+                search.
               </p>
-              <a href="/#" className="special">
+              <a
+                href="https://dc.library.northwestern.edu/"
+                className="special"
+              >
+                Learn more
+              </a>
+            </article>
+
+            <article>
+              <a href="http://www.urgeoverkill.com/" className="image">
+                <img src={portfolioUO} alt="" />
+              </a>
+              <h3 className="major">Urge Overkill</h3>
+              <p>
+                A smattering of WordPress plugins, some custom PHP modules and
+                CSS drive Chicago's indie darlings, Urge Overkill's website.
+              </p>
+              <a href="http://www.urgeoverkill.com/" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a
+                href="https://www.npmjs.com/package/openseadragon-react-viewer"
+                className="image"
+              >
+                <img
+                  src={portfolioOSD}
+                  alt="OpenSeadragon React Viewer screen shot"
+                />
+              </a>
+              <h3 className="major">OpenSeadragon React Viewer NPM Package</h3>
+              <p>
+                NPM package React component designed as a wrapper around the
+                OpenSeadragon viewer. If your application supports tiled images
+                for deep zoom, import this component into your React
+                application, feed it a IIIF manifest URL via props and it will
+                render an OpenSeadragon viewer with custom navigation, filtering
+                of tile sources, and zoom/pan controls.
+              </p>
+              <a
+                href="https://www.npmjs.com/package/openseadragon-react-viewer"
+                className="special"
+              >
+                Learn more
+              </a>
+            </article>
+
+            <article>
+              <a href="https://www.morningstar.com/" className="image">
+                <img src={portfolioMorningstar} alt="" />
+              </a>
+              <h3 className="major">Morningstar Investments</h3>
+              <p>
+                ES5 JavaScript custom framework, AEM (Adobe Experience
+                Mananger), CSS3/HTML.
+              </p>
+              <a href="https://www.morningstar.com/" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="https://www.avalonmediasystem.org/" className="image">
+                <img src={portfolioAvalon} alt="" />
+              </a>
+              <h3 className="major">Avalon Media System</h3>
+              <p>
+                Audio / Video repository application co-developed by Indiana and
+                Northwestern Universities. A Rails application, where I
+                re-vamped the entire JavaScript implementation into ES6+
+                modules, and began a UI component pattern using ReactJS and
+                Webpack.
+              </p>
+              <a href="https://www.avalonmediasystem.org/" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="https://thelastvegas.com/" className="image">
+                <img src={portfolioTLV} alt="" />
+              </a>
+              <h3 className="major">The Last Vegas</h3>
+              <p>
+                GatsbyJS, React, and HTMLUP tool this website for Chicago's
+                garage, glam boogie rock faves.
+              </p>
+              <a href="https://thelastvegas.com/" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a
+                href="https://arch.library.northwestern.edu/"
+                className="image"
+              >
+                <img src={portfolioArch} alt="" />
+              </a>
+              <h3 className="major">ARCH: Institutional Repository</h3>
+              <p>
+                Arch is Northwestern Libraries Institutional Repository,
+                designed for self-deposit of works including research, thesis,
+                and dissertations by faculty and students. Built on top of the
+                Samvera community's Hyrax application, I customized this Rails
+                application to adhere to Northwestern Global Marketing branding
+                guidelines.
+              </p>
+              <a
+                href="https://arch.library.northwestern.edu/"
+                className="special"
+              >
                 Learn more
               </a>
             </article>
